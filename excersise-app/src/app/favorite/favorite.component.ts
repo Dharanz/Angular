7ec7ab641c8e57acd1fef1e4049df3808f00ae4e
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2017-12-01 14:28:52 +0530

    updated

--- excersise-app/src/app/favorite/favorite.component.ts
+++ excersise-app/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  isFavorite:boolean;
+  @Input('isFavorite') isSelected:boolean;
+  @Output() change=new EventEmitter();
 
   constructor() { }
 
@@ -15,10 +16,11 @@ export class FavoriteComponent implements OnInit {
   }
 
   onclick(){
-    this.isFavorite=!this.isFavorite;
+    this.isSelected=!this.isSelected;
+    this.change.emit();
   }
 
   getStyle(){
-    return this.isFavorite ? "visible" : "hidden";
+    return this.isSelected ? "visible" : "hidden";
   }
 }
