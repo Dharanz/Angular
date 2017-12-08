9efce89c01a8dbb82a8997304ab8a068a4244c44
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2017-12-08 17:41:46 +0530

    form-app

--- excersise-app/src/app/zippy/zippy.component.ts
+++ excersise-app/src/app/zippy/zippy.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-zippy',
+  selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css']
 })
-export class ZippyComponent implements OnInit {
+export class ZippyComponent {
 
-  constructor() { }
+  @Input('isActive') isActive:boolean;
+  @Input('isActiveContent') isActiveContent:boolean;
+  @Input('title') title:string="Shipping Details";
+  @Input('billingtTitle') billingtTitle:string="Billing Details";
 
-  ngOnInit() {
+  toggle(){
+      if(this.isActiveContent){
+        this.isActiveContent=false;
+        this.isActive=true;
+      }else{
+        this.isActive=!this.isActive;
+      }
   }
 
+  contentToggle(){
+    if(this.isActive){
+      this.isActive=false;
+      this.isActiveContent=true;
+    }else{
+      this.isActiveContent=!this.isActiveContent;
+    }
+  }
 }
