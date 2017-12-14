1f1adf450d4b226133400a1d7ce787016a382409
Dharanz <dharanm387@gmail.com>
2017-12-14 08:21:52 -0800

    updated

--- forms-app/src/app/signup-form/signup-form.component.ts
+++ forms-app/src/app/signup-form/signup-form.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserNameValidator } from './username.validators';
 
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent {
 
   form=new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    // username: new FormControl('', [
+    //   Validators.required,
+    //   Validators.minLength(7),
+    //   Validators.pattern('Angular4'),
+    //   UserNameValidator.CannotContainSpace
+    // ]),
+    username: new FormControl('', Validators.required, UserNameValidator.ShouldBeUnique),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
   });
 
-  constructor() { }
-
-  ngOnInit() {
+  log(x){
+    console.log(x);
   }
 
+  get username(){
+    return this.form.get('username');
+  }
+  get password(){
+    return this.form.get('password');
+  }
 }
