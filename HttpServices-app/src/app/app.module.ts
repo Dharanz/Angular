a399fbfd14f0faf19cfa6e6ab4ce17de0fcd9698
Dharanz <dharanm387@gmail.com>
2017-12-22 08:58:34 -0800

    new app

---
+++ HttpServices-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ HttpServices-app/src/app/posts/posts.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {
+
+  posts: any[];
+
+  private url = 'http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: Http) { 
+    http.get(this.url)
+      .subscribe(response => {
+        this.posts = response.json();
+      });
+  }
+
+
+  addObject(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+    
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+        
+         console.log(response.json());
+      })
+  }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead:true}))
+      .subscribe(response => {
+        let index=this.posts.indexOf(post);
+        post.title = "updated";
+        // post.title
+      });
+  }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(resonse => {
+        let index= this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+  }
+}
