67f55d4c3eb5d476277bccc05529cc27df680cb2
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2018-01-09 12:06:08 +0530

    added

--- excersise-app/src/app/app.module.ts
+++ excersise-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { LikeComponent } from './like/like.component';
 import { DirectiveExamplesComponent } from './directive-examples/directive-examples.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -24,11 +26,19 @@ import { ZippyComponent } from './zippy/zippy.component';
     LikeComponent,
     DirectiveExamplesComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: TitleCaseComponent},
+      { path: 'favorite', component: FavoriteComponent},
+      { path: 'panel', component: PanelComponent},
+      { path: 'like', component: LikeComponent},
+      { path: 'zippy', component: ZippyComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
