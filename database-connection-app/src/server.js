c1af3d3933c6cc397c366d8e0f76c5d23ecc0f76
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2018-01-09 17:56:10 +0530

    updated

--- database-connection-app/src/server.js
+++ database-connection-app/src/server.js
@@ -1,38 +1,65 @@
-var express = require('express');
-var app = express();
+//Initiallising node modules
+var express = require("express");
+var bodyParser = require("body-parser");
+var sql = require("mssql");
+var app = express(); 
 
-app.get('/', function (req, res) {
-   
-    var sql = require("mssql");
+// Body Parser Middleware
+app.use(bodyParser.json()); 
 
-    // config for your database
-    var config = {
-        user: 'sa',
-        password: 'Shlok123',
-        server: 'SHLOK16\\SQL2012', 
-        database: 'SampleDB' 
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from UserDetails', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
 });
 
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
+//Setting up server
+ var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+ });
+
+//Initiallising connection string
+var dbConfig = {
+    user:  'sa',
+    password: 'Shlok123',
+    server: 'SHLOK16\\\SQL2012',
+    database:'SampleDB',
+    options: {
+        encrypt: true
+      }
+};
+
+//Function to connect to database and execute query
+var  executeQuery = function(res, query){             
+     sql.connect(dbConfig, function (err) {
+         if (err) {   
+                     console.log("Error while connecting database :- " + err);
+                     res.send(err);
+                  }
+                  else {
+                         // create Request object
+                         var request = new sql.Request();
+                         // query to the database
+                         request.query(query, function (err, res) {
+                           if (err) {
+                                      console.log("Error while querying database :- " + err);
+                                      res.send(err);
+                                     }
+                                     else {
+                                       res.send(res);
+                                            }
+                               });
+                               sql.close();
+                       }
+      });           
+}
+
+//GET API
+app.get("/user", function(req , res){
+                var query = "select * from UserDetails";
+                executeQuery (res, query);
 });
