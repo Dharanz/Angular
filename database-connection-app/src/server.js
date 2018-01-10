1691c14d86b42c1acac6b541a47f18c1d8914d00
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2018-01-10 15:23:20 +0530

    updated

--- database-connection-app/src/server.js
+++ database-connection-app/src/server.js
@@ -1,65 +1,47 @@
-//Initiallising node modules
-var express = require("express");
-var bodyParser = require("body-parser");
-var sql = require("mssql");
-var app = express(); 
+var express = require('express');
+var app = express();
 
-// Body Parser Middleware
-app.use(bodyParser.json()); 
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
 
-//CORS Middleware
-app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'Shlok123',
+        server: '198.168.0.124', 
+        options: {
+            database: 'SampleDB',
+            instancename: 'SQL2012'
+          } 
+    };
+
+    // connect to your database
+    if(sql.connect(config)){
+        console.log(config);
+    }else{
+        console.log("ERROR")
+    }
+
+    // sql.connect(config, function (err) {
+    
+    //     if (err) console.log(err);
+
+    //     // create Request object
+    //     var request = new sql.Request();
+           
+    //     // query to the database and get the records
+    //     request.query('select * from UserDetails', function (err, recordset) {
+            
+    //         if (err) console.log(err)
+
+    //         // send records as a response
+    //         res.send(recordset);
+            
+    //     });
+    // });
 });
 
-//Setting up server
- var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
- });
-
-//Initiallising connection string
-var dbConfig = {
-    user:  'sa',
-    password: 'Shlok123',
-    server: 'SHLOK16\\\SQL2012',
-    database:'SampleDB',
-    options: {
-        encrypt: true
-      }
-};
-
-//Function to connect to database and execute query
-var  executeQuery = function(res, query){             
-     sql.connect(dbConfig, function (err) {
-         if (err) {   
-                     console.log("Error while connecting database :- " + err);
-                     res.send(err);
-                  }
-                  else {
-                         // create Request object
-                         var request = new sql.Request();
-                         // query to the database
-                         request.query(query, function (err, res) {
-                           if (err) {
-                                      console.log("Error while querying database :- " + err);
-                                      res.send(err);
-                                     }
-                                     else {
-                                       res.send(res);
-                                            }
-                               });
-                               sql.close();
-                       }
-      });           
-}
-
-//GET API
-app.get("/user", function(req , res){
-                var query = "select * from UserDetails";
-                executeQuery (res, query);
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
 });
