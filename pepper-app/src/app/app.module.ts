643e30e833b0052f7fe59431197a0840fe7b0425
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2018-01-27 17:52:31 +0530

    pepper

---
+++ pepper-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+
+import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRzZE2ipLFnLTQbGjInL0HsUodC0Xb-j4",
+  authDomain: "pepper-29a74.firebaseapp.com",
+  databaseURL: "https://pepper-29a74.firebaseio.com",
+  projectId: "pepper-29a74",
+  storageBucket: "pepper-29a74.appspot.com",
+  messagingSenderId: "882316859897"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
