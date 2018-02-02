8bb8b72de2118d519d81648d1a54a6ea4666d514
muralidharan.m <muralidharan.m@SHLOKINDIA.COM>
2018-02-02 11:49:33 +0530

    CRUD

--- pepper-app/src/app/app.component.ts
+++ pepper-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { AngularFire } from 'angularfire2';
 export class AppComponent {
   title = 'app works!';
 
-  cuisines;
+  cuisines: FirebaseListObservable<any[]>;
   restaurant;
 
   constructor(private af: AngularFire){
@@ -18,5 +18,27 @@ export class AppComponent {
   ngOnInit(){
     this.cuisines = this.af.database.list('/cuisines');
     this.restaurant = this.af.database.object('/restaurant');
+  }
+
+  add(){
+    this.cuisines.push({
+      name: 'Asian',
+      Details: {
+        description: '...'
+      }
+    });
+  }
+
+  update(){
+    this.restaurant.set({
+      name: 'New Name',
+      rating: 5
+    });
+  }
+
+  remove(){
+    this.af.database.object('/restaurant').remove()
+      .then(x=> console.log("SUCCESS"))
+      .catch(error => console.log("ERROR", error));
   }
 }
